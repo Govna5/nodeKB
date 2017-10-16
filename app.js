e8b18f0f86618bf740f45ffe7b9f2ddf66b89b02
Julian Governale <julian@Batcave.local>
2017-10-15 23:01:16 -0400

    added express sessions, mesages and valdiation with content-flash

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-Parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 
 // connect to the database (mongodb)
@@ -26,8 +29,10 @@ db.on('error', function(err){
 // Init App
 const app = express();
 
+
 // Bring in models
 let Article = require('./models/article');
+
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -38,12 +43,46 @@ app.set('view engine', 'pug')
 //parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 // parse application/json
 app.use(bodyParser.json());
+
 
 // Set Public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Set up Express-session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter : function(param, msg, vlaue) {
+    var namespace = param.split('.')
+    , root = namespace.shift()
+    , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return{
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}));
 
 
 ///// Routes  //////
@@ -62,7 +101,7 @@ app.get('/', function(req, res){
   });
 });
 
-//Get Single Routes
+//Get Article Routes
 app.get('/article/:id', function(req, res){
   Article.findById(req.params.id, function(err, article){
     res.render('article',{
@@ -72,7 +111,7 @@ app.get('/article/:id', function(req, res){
 });
 
 
-// Add Routes
+// Add Article Routes
 app.get('/articles/add', function(req, res){
   res.render('add_article', {
     title:'Add Article'
@@ -86,6 +125,9 @@ app.get('/contact', function(req, res){
   });
 });
 
+
+////  GET , POST, DElELTE Routes ////
+
 //Add Submit POST Routes
 app.post('/articles/add', function(req, res){
   let article = new Article();
@@ -98,11 +140,59 @@ app.post('/articles/add', function(req, res){
       console.log(err);
       return
     } else {
+      req.flash('success', 'Artilce Added');
       res.redirect('/');
     }
   });
 });
 
+//Update Submit Routes
+app.post('/articles/edit/:id', function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      console.log(req.body);
+      //req.flash('Success, Artilce Added');
+      res.redirect('/');
+    }
+  });
+});
+
+
+
+
+//Delete Article Routes
+app.delete('/article/:id', function(req, res){
+  let query = {_id:req.params.id}
+
+  Article.remove(query, function(err){
+    if(err){
+      console.log(err);
+    }
+    req.flash('danger', 'Article Deleted');
+    res.send('Success');
+  });
+});
+
+
+// Load Edit form
+app.get('/article/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article',{
+      title:'Edit Article',
+      article:article
+    });
+  });
+});
 
 // Start Server
 app.listen(3000, function () {
